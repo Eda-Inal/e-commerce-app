c9290a4c6cfe56a8697c8474d861a46706c3b37c
Eda Işıl İnal <edainalisil@gmail.com>
2024-09-09 10:07:14 +0300

    add css home page

--- commerce/src/app/home/page.jsx
+++ commerce/src/app/home/page.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 import Image from 'next/image'
 import { Box,Text,Button } from '@chakra-ui/react'
 import img from "../../../public/home.png"
-import { RiDoubleQuotesL ,RiDoubleQuotesR} from "react-icons/ri";
+import { SlBasket } from "react-icons/sl";
 
 
 
@@ -13,32 +13,41 @@ function Main() {
  
   <Box width="100%" height="60vh" bgGradient='linear(to-r,#F8BBD0, #F48FB1)' >
     <Box width="80%" display="flex" mt={3} justifyContent="space-around" margin="0 auto">
-        <Box width="50%" position="relative"  zIndex={100}>
-        <Box width="300px" height="405px" position="relative" mt={3}>
-  <Image 
-    src={img}
-    layout="fill"
-    objectFit="cover" 
-    alt="Image description"
-  />
+    <Box width="50%">
+  <Box  mt={5}
+    width={400} 
+    height={300} 
+    borderRadius="50%" 
+    bgColor="#F36394" 
+    filter="blur(80px)" 
+    position="absolute" 
+    zIndex={0} 
+  >
+  </Box>
+  
+  <Box width="300px" height="405px" position="relative" mt={3} zIndex={1}ml={5}> 
+    <Image 
+      src={img}
+      layout="fill"
+      objectFit="cover" 
+      alt="Image description"
+    />
+  </Box>
 </Box>
-        </Box>
+
 {/* *******content***** */}
 
 
         <Box width="50%" mt={5} position="relative"  zIndex={100}>
-      <Text textColor="white" fontSize="4xl" >Reveal Your Radiant Skin </Text>
-      <Text fontSize="4xl" textColor="#323233">Today</Text>
-      <Text mt={3} fontSize="20px" textColor="white">Discover the Secrets to Glowing Skin</Text>
-      <Button mt={5} width={120} bgColor="#323233" p={2}><Text textColor="white">Shop now</Text></Button>
+      <Text textColor="white" fontSize="4xl" mt={8}>Reveal Your Radiant Skin Today </Text>
+      <Text mt={8} mb={8} fontSize="20px" textColor="white">Discover the Secrets to Glowing Skin</Text>
+      <Button rightIcon={<SlBasket color="white" />}  width={140} bgColor="#323233"    _hover={{bgColor: 'transparent',  border: '1px solid', borderColor: "#323233", 
+  }}><Text textColor="white">Shop now</Text></Button>
            
         </Box>
     </Box>
   </Box>
-  <Box position="absolute" width={400} height={300} top="70px" left="100px" borderRadius="50%" bgColor="#F36394" blur="100px"   filter="blur(100px)">
-  </Box>
-  <Box position="absolute" width={400} height={300} top="-178px" left="747px" borderRadius="50%" bgColor="#F36394" blur="100px"   filter="blur(100px)">
-  </Box>
+ 
   </>
   )
 }
